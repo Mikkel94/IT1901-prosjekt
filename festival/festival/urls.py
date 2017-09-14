49252ca85bb5c63c947e6d397dd2d5c15b1981d0
Mikkel Sandsbråten <mikkelsandsbraten@dhcp-10-22-49-221.wlan.ntnu.no>
2017-09-14 09:51:32 +0200

    some more

--- festival/festival/urls.py
+++ festival/festival/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from festivalapp.views import index
 
 urlpatterns = [
+    url(r'^$', index, name='landingpage'),
     url(r'^admin/', admin.site.urls),
-    url(r'festivalapp/', include('festivalapp.urls'))
+    url(r'festivalapp/', include('festivalapp.urls')),
 ]
