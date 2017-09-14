a4cb05150fbe6ec5abf8ac79260066f7f42b857f
Mikkel Sandsbråten <mikkelsandsbraten@dhcp-10-22-49-221.wlan.ntnu.no>
2017-09-14 10:09:26 +0200

    gitignore

--- festival/festivalapp/admin.py
+++ festival/festivalapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Band, Concert
 
 # Register your models here.
+admin.site.register(Band)
+admin.site.register(Concert)
--- festival/festivalapp/models.py
+++ festival/festivalapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Band(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Concert(models.Model):
+    band = models.OneToOneField(Band)
+
+    def __str__(self):
+        return self.band.name
