b3ee908548c6c6709fe8256d728455a91224ddca
Mikkel Sandsbråten <mikkelsandsbraten@dhcp-10-22-49-221.wlan.ntnu.no>
2017-09-14 10:33:36 +0200

    small changes

--- festival/festivalapp/models.py
+++ festival/festivalapp/models.py
@@ -13,3 +13,4 @@ class Concert(models.Model):
 
     def __str__(self):
         return self.band.name
+
--- festival/festivalapp/urls.py
+++ festival/festivalapp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import index
 
+app_name = 'festivalapp'
 urlpatterns = [
     url(r'^$', index, name='index')
 ]
