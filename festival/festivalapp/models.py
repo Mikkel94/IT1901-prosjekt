4c5527059a1768236249e2019d7cbe26be09f371
Mikkel Sandsbråten <mikkelsandsbraten@dhcp-10-22-35-83.wlan.ntnu.no>
2017-09-18 11:20:39 +0200

    dropdown menu in register added

--- festival/festivalapp/models.py
+++ festival/festivalapp/models.py
@@ -2,9 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATUS_CHOICES = (
+    ('arrangør', 'ARRANGØR'),
+    ('lydtekniker', 'LYDTEKNIKER'),
+    ('lystekniker', 'LYSTEKNIKER'),
+)
+
 class Employee(models.Model):
     user = models.OneToOneField(User)
-    employee_status = models.CharField(max_length=32)
+    employee_status = models.CharField(max_length=32, choices=STATUS_CHOICES)
 
     def __str__(self):
         return self.user.username
