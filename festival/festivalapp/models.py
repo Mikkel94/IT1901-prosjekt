0c9433ea5feb5c5aaa4258aab390064077a7583f
Truls <trulsabe@stud.ntnu.no>
2017-09-20 23:59:01 +0200

    fixed with Concert model, still some problems

--- festival/festivalapp/models.py
+++ festival/festivalapp/models.py
@@ -17,17 +17,21 @@ class Employee(models.Model):
     def __str__(self):
         return self.user.username
 
-# concert model
 
-
-class Concert(models.Model):
-    name = models.CharField(max_length=50)
-    band = models.CharField(max_length=40)
-    date = models.DateField()
-    scenes = models.ForeignKey('Scene')
+class Band(models.Model):
+    name = models.CharField(max_length=60, default="BandX")
+    members = models.IntegerField(default=0)
 
 
 class Scene(models.Model):
     id = models.AutoField(primary_key=True)
+    
 
-# concert model end
+class Concert(models.Model):
+    name = models.CharField(max_length=50)
+    bands = models.ForeignKey(Band)
+    date = models.DateField()
+    scenes = models.ForeignKey(Scene)
+
+
+
