521274030d689d24904685abcba7bbb24d23b2ab
Mikkel Sandsbråten <mikkelsandsbraten@Mikkels-MacBook-Pro.local>
2017-10-04 13:38:03 +0200

    booking_responsible can book bands, TODO: send email to manager

--- festival/festivalapp/urls.py
+++ festival/festivalapp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from .views import index, user_login, register, list_concert, user_logout, manager
+from .views import index, user_login, register, list_concert,\
+    user_logout, manager, booking_responsible, book_band, delete_band
 
 app_name = 'festivalapp'
 urlpatterns = [
@@ -8,6 +9,8 @@ urlpatterns = [
     url(r'^logout/$', user_logout, name='logout'),
     url(r'^register/$', register, name='register'),
     url(r'^concert_list/$', list_concert, name='list_concert'),
-    url(r'^manager/$', manager, name='manager')
-
+    url(r'^manager/$', manager, name='manager'),
+    url(r'^booking_responsible/$', booking_responsible, name='booking_responsible'),
+    url(r'^booking/(?P<pk>\d+)/add$', book_band, name='booking'),
+    url(r'^bands/(?P<pk>\d+)/remove/$', delete_band, name='delete_band'),
 ]
